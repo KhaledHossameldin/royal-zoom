7eb8fc03c561fad0013e94ac5ad540ec62aaa50e
Khaled Hossameldin <khaled.hossameldin99@gmail.com>
2024-02-07 17:35:19 +0200

    feature: add new major request improvement (Khaled)

--- lib/data/sources/consultant/major/major_remote_data_source.dart
+++ lib/data/sources/consultant/major/major_remote_data_source.dart
@@ -3,32 +3,17 @@ import '../../../../core/models/empty_model.dart';
 import '../../../../core/network/endpoints/network.dart';
 import '../../../../core/network/http_method.dart';
 import '../../../../core/results/result.dart';
-import '../../../../utilities/extensions.dart';
+import '../../../models/consultants/add_new_major_request_body.dart';
 
 class MajorRemoteDataSource {
   Future<Result<EmptyModel>> addNewMajorRequest({
-    required int majorId,
-    required bool isActive,
-    required String yearsOfExperience,
-    required String price,
-    required String terms,
-    required bool isNotificationsEnabled,
-    required String consultantPreviewName,
+    required AddNewMajorRequestBody body,
   }) async {
     return await RemoteDataSource.request(
       converter: (model) => EmptyModel(model),
       method: HttpMethod.POST,
       url: Network.newMajorRequests,
-      data: {
-        'major_id': majorId.toString(),
-        'is_active': isActive.toInt.toString(),
-        'years_of_experience': yearsOfExperience.toString(),
-        'price_per_hour': price.toString(),
-        'terms': terms,
-        'is_notifications_enabled': isNotificationsEnabled.toInt.toString(),
-        'is_free': 0,
-        'consultant_preview_name': consultantPreviewName,
-      },
+      data: body.toMap(),
     );
   }
 }
